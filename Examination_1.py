100c171a0dcfdb68902e299e6931001a383c90d9
sarisaghty <28156@ud.ac.th>
2026-09-17 09:52:29 +0700

    Improve input prompts and fix total calculation
    Updated input prompts for better clarity and fixed total calculation.

--- Examination_1.py
+++ Examination_1.py
@@ -6,5 +6,7 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-pay = int(input())
-price = int(input())
+pay = int(input("จำนวนเงินที่จ่าย: "))
+price = int(input("ราคาสินค้า: "))
+total = int(input(pay-price))
+print("จำนวนเงินทอน:, total")
